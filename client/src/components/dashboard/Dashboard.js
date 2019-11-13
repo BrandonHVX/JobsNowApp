3ee8f50ae53330451b1723272a51b865e0746307
Brandon <brandongines@gmail.com>
2019-11-13 12:26:17 -0500

    dash

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -12,6 +12,14 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import jobsnowbg from "../../img/cooljobsbg.jpg";
+import ProfileTop from '../profile/ProfileTop';
+
+
+
+
+
+
+
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -29,11 +37,30 @@ const useStyles = makeStyles(theme => ({
   },
   background: {
     backgroundImage: "red",
-    backgroundPosition: "center",
+    minHeight: '600px',
+    backgroundPosition: "center top",
+    backgroundSize: "100%",
     backgroundImage: `url(${jobsnowbg})`,
-    backgroundRepeat: "no-repeat"
+    backgroundRepeat: "no-repeat",
+
   }
+
+
+
 }));
+
+
+const headerbackground = {
+
+  backgroundPosition: "center ",
+
+  backgroundImage: `url(${jobsnowbg})`,
+  backgroundRepeat: "no-repeat",
+
+}
+
+
+
 
 const Dashboard = ({
   getCurrentProfile,
@@ -49,181 +76,103 @@ const Dashboard = ({
   return loading && profile === null ? (
     <Spinner />
   ) : (
-    <Fragment>
+      <Fragment>
 
-      <Paper className={classes.background}>
-      <div class="container-fluid mt--7">
-<div class="row">
-<div class="col-xl-4 order-xl-2 mb-5 mb-xl-0">
-<div class="card card-profile shadow">
-<div class="row justify-content-center">
-<div class="col-lg-3 order-lg-2">
-<div class="card-profile-image">
-<a href="#">
-<img src="https://raw.githack.com/creativetimofficial/argon-dashboard/master/assets/img/theme/team-4-800x800.jpg" class="rounded-circle"/>
-</a>
-</div>
-</div>
-</div>
-<div class="card-header text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
-<div class="d-flex justify-content-between">
-<a href="#" class="btn btn-sm btn-info mr-4">Connect</a>
-<a href="#" class="btn btn-sm btn-default float-right">Message</a>
-</div>
-</div>
-<div class="card-body pt-0 pt-md-4">
-<div class="row">
-<div class="col">
-<div class="card-profile-stats d-flex justify-content-center mt-md-5">
-<div>
-<span class="heading">22</span>
-<span class="description">Friends</span>
-</div>
-<div>
-<span class="heading">10</span>
-<span class="description">Photos</span>
-</div>
-<div>
-<span class="heading">89</span>
-<span class="description">Comments</span>
-</div>
-</div>
-</div>
-</div>
-<div class="text-center">
-<h3>
-Jessica Jones<span class="font-weight-light">, 27</span>
-</h3>
-<div class="h5 font-weight-300">
-<i class="ni location_pin mr-2"></i>Bucharest, Romania
-</div>
-<div class="h5 mt-4">
-<i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim Officer
-</div>
-<div>
-<i class="ni education_hat mr-2"></i>University of Computer Science
-</div>
+        <div>
+          <div class="header pb-8 pt-5 pt-lg-8 d-flex align-items-center" style={headerbackground} >
 
-<p>Ryan the name taken by Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and records all of his own music.</p>
-<a href="#">Show more</a>
-</div>
-</div>
-</div>
-</div>
-<div class="col-xl-8 order-xl-1">
-<div class="card bg-secondary shadow">
-<div class="card-header bg-white border-0">
-<div class="row align-items-center">
-<div class="col-8">
-<h3 class="mb-0">My account</h3>
-</div>
-<div class="col-4 text-right">
-<a href="#!" class="btn btn-sm btn-primary">Settings</a>
-</div>
-</div>
-</div>
-<div class="card-body">
-<form>
-<h6 class="heading-small text-muted mb-4">User information</h6>
-<div class="pl-lg-4">
-<div class="row">
-<div class="col-lg-6">
-<div class="form-group focused">
-<label class="form-control-label" for="input-username">Username</label>
-<input type="text" id="input-username" class="form-control form-control-alternative" placeholder="Username" value="lucky.jesse"/>
-</div>
-</div>
-<div class="col-lg-6">
-<div class="form-group">
-<label class="form-control-label" for="input-email">Email address</label>
-<input type="email" id="input-email" class="form-control form-control-alternative" placeholder="jesse@example.com"/>
-</div>
-</div>
-</div>
-<div class="row">
-<div class="col-lg-6">
-<div class="form-group focused">
-<label class="form-control-label" for="input-first-name">First name</label>
-<input type="text" id="input-first-name" class="form-control form-control-alternative" placeholder="First name" value="Lucky"/>
-</div>
-</div>
-<div class="col-lg-6">
-<div class="form-group focused">
-<label class="form-control-label" for="input-last-name">Last name</label>
-<input type="text" id="input-last-name" class="form-control form-control-alternative" placeholder="Last name" value="Jesse"/>
-</div>
-</div>
-</div>
-</div>
+            <span class="mask bg-gradient-default opacity-1" />
+
+            <div class="container-fluid d-flex align-items-center">
+              <div class="row">
+                <div class="col-lg-7 col-md-10">
+                  <h1 class="display-2 text-white">Hello, {user && user.name}</h1>
+                  <p class="text-white mt-0 mb-5">This is your profile page. You can see the progress you've made with your work and manage your projects or assigned tasks</p>
+
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="container-fluid mt--7">
+            <div class="row">
+              <div class="col-xl-4 order-xl-1 mb-5 mb-xl-0">
+                <div class="card card-profile shadow">
+                  <div class="row justify-content-center">
+                    <div class="col-lg-3 order-lg-2">
+                      <div class="card-profile-image">
+
+                        <img src={user && user.avatar} style={{ width: '200px' }} class="rounded-circle" />
+
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="card-body pt-5 pt-md-4 mt-5">
+                    <div class="row">
+                      <div class="col">
+                        <div class="card-profile-stats d-flex justify-content-center mt-md-5">
 
 
-<h6 class="heading-small text-muted mb-4">Contact information</h6>
-<div class="pl-lg-4">
-<div class="row">
-<div class="col-md-12">
-<div class="form-group focused">
-<label class="form-control-label" for="input-address">Address</label>
-<input id="input-address" class="form-control form-control-alternative" placeholder="Home Address" value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09" type="text"/>
-</div>
-</div>
-</div>
-<div class="row">
-<div class="col-lg-4">
-<div class="form-group focused">
-<label class="form-control-label" for="input-city">City</label>
-<input type="text" id="input-city" class="form-control form-control-alternative" placeholder="City" value="New York"/>
-</div>
-</div>
-<div class="col-lg-4">
-<div class="form-group focused">
-<label class="form-control-label" for="input-country">Country</label>
-<input type="text" id="input-country" class="form-control form-control-alternative" placeholder="Country" value="United States"/>
-</div>
-</div>
-<div class="col-lg-4">
-<div class="form-group">
-<label class="form-control-label" for="input-country">Postal code</label>
-<input type="number" id="input-postal-code" class="form-control form-control-alternative" placeholder="Postal code"/>
-</div>
-</div>
-</div>
-</div>
+
+                        </div>
+                      </div>
+                    </div>
+                    <div class="text-center">
+                      <h4>
+                        {user && user.name}
+                      </h4>
+                      <div class="h5 font-weight-300">
+                        <i class="ni location_pin mr-2"></i>
+                      </div>
 
 
-<h6 class="heading-small text-muted mb-4">About me</h6>
-<div class="pl-lg-4">
-<div class="form-group focused">
-<label>About Me</label>
-<textarea rows="4" class="form-control form-control-alternative" placeholder="A few words about you ...">A beautiful Dashboard for Bootstrap 4. It is Free and Open Source.</textarea>
-</div>
-</div>
-</form>
-</div>
-</div>
-</div>
-</div>
-</div>
 
-      </Paper>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xl-8 order-xl-2">
+                <div class="card bg-secondary shadow">
+                  <div class="card-header bg-white border-0">
+                    <div class="row align-items-center">
+                      <div class="col-8">
+                        <h3 class="mb-0">My account</h3>
+                      </div>
+                      <div class="text-center">
+                        <DashboardActions />
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card-body">
 
-      {profile !== null ? (
-        <Fragment>
-          {/* <div className="my-2">
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+
+        {profile !== null ? (
+          <Fragment>
+            {/* <div className="my-2">
             <button className="btn btn-danger" onClick={() => deleteAccount()}>
               <i className="fas fa-user-minus" /> Delete My Account
             </button>
           </div> */}
-        </Fragment>
-      ) : (
-        <Fragment>
-          <p>You have not yet setup a profile, please add some info</p>
-          <Link to="/create-profile" className="btn btn-primary my-1">
-            Create Profile
+          </Fragment>
+        ) : (
+            <Fragment>
+              <p>You have not yet setup a profile, please add some info</p>
+              <Link to="/create-profile" className="btn btn-primary my-1">
+                Create Profile
           </Link>
-        </Fragment>
-      )}
-    </Fragment>
-  );
+            </Fragment>
+          )}
+      </Fragment>
+    );
 };
 
 Dashboard.propTypes = {
--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -4,17 +4,20 @@ import { Link } from 'react-router-dom'
 const DashboardActions = () => {
   return (
     <div className="dash-buttons">
-      <Link to="/edit-profile" className="btn btn-light">
-        <i className="fas fa-user-circle text-primary" /> Edit Profile
+
+
+
+      <Link to="/edit-profile" class="btn btn-sm btn-primary">
+        <i className="fas fa-user-circle " /> Edit Profile
       </Link>
-      <Link to="/add-experience" className="btn btn-light">
-        <i className="fab fa-black-tie text-primary" /> Add Experience
+      <Link to="/add-experience" class="btn btn-sm btn-primary">
+        <i className="fab fa-black-tie" /> Add Experience
       </Link>
-      <Link to="/add-education" className="btn btn-light">
-        <i className="fas fa-graduation-cap text-primary" /> Add Education
+      <Link to="/add-education" class="btn btn-sm btn-primary">
+        <i className="fas fa-graduation-cap" /> Add Education
       </Link>
-      <Link to="/add-post" className="btn btn-light">
-        <i className="fas fa-graduation-cap text-primary" /> Add Post
+      <Link to="/add-post" class="btn btn-sm btn-primary">
+        <i className="fas fa-graduation-cap" /> Add Post
       </Link>
     </div>
   )
